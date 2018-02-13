60fd112f8cb8a997ea66fba0a5c0c9fe1f890618
Weinan Li <l.weinan@gmail.com>
2018-02-14 00:58:19 +0800

    add foo app

--- src/main/java/io/weli/FooApplication.java
+++ src/main/java/io/weli/FooApplication.java
@@ -1,6 +1,20 @@
 package io.weli;
 
 import javax.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
 
 public class FooApplication extends Application {
+    @Override
+    public Set<Class<?>> getClasses() {
+        HashSet<Class<?>> set = new HashSet<Class<?>>();
+        set.add(io.swagger.jaxrs.listing.ApiListingResource.class);
+        set.add(io.swagger.jaxrs.listing.SwaggerSerializers.class);
+        return set;
+    }
+
+    @Override
+    public Set<Object> getSingletons() {
+        return super.getSingletons();
+    }
 }
