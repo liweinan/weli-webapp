430549fb655f4120b1696696d899ccaedb2fe536
Weinan Li <l.weinan@gmail.com>
2018-02-14 01:07:03 +0800

    try singleton

--- src/main/java/io/weli/FooApplication.java
+++ src/main/java/io/weli/FooApplication.java
@@ -5,17 +5,22 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class FooApplication extends Application {
+
+    HashSet<Object> singletons = new HashSet<Object>();
+
     @Override
     public Set<Class<?>> getClasses() {
         HashSet<Class<?>> set = new HashSet<Class<?>>();
         set.add(FooResource.class);
-        set.add(io.swagger.jaxrs.listing.ApiListingResource.class);
-        set.add(io.swagger.jaxrs.listing.SwaggerSerializers.class);
+//        set.add(io.swagger.jaxrs.listing.ApiListingResource.class);
+//        set.add(io.swagger.jaxrs.listing.SwaggerSerializers.class);
         return set;
     }
 
     @Override
     public Set<Object> getSingletons() {
-        return super.getSingletons();
+        singletons.add(new io.swagger.jaxrs.listing.ApiListingResource());
+        singletons.add(new io.swagger.jaxrs.listing.SwaggerSerializers());
+        return singletons;
     }
 }
