4512bc6a7cda9bfde76e4f8ac80e33cd839e9abc
Weinan Li <l.weinan@gmail.com>
2018-02-14 09:17:06 +0800

    foo resource

--- src/main/java/io/weli/FooResource.java
+++ src/main/java/io/weli/FooResource.java
@@ -1,9 +1,12 @@
 package io.weli;
 
+import io.swagger.annotations.Api;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
 @Path("/foo")
+@Api(value = "/foo", description = "foo resource")
 public class FooResource {
 
     @GET
